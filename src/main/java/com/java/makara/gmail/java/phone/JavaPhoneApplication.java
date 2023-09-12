175411193c769de818e30d265c0a7faeba71892b
DESKTOP-NFBVRP4\Lenovo <doemmakarask@gmail.com>
2023-09-12 15:29:42 +0700

    first commit

---
+++ src/main/java/com/java/makara/gmail/java/phone/JavaPhoneApplication.java
@@ -0,0 +1,13 @@
+package com.java.makara.gmail.java.phone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPhoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/makara/gmail/java/phone/JavaPhoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.makara.gmail.java.phone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPhoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
